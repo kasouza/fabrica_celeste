d8fbf67c745e835b4a00c1a72d4c158f5c713944
kaso <kaua.scmm@gmail.com>
2025-01-11 10:28:29 -0300

    Remove cross product from vec3i

--- fabrica/math/vec3i.c
+++ fabrica/math/vec3i.c
@@ -26,10 +26,3 @@ void fabrica_vec3i_scale(const fabrica_Vec3I *a, int32_t scalar,
     out->y = a->y * scalar;
     out->z = a->z * scalar;
 }
-
-void fabrica_vec3i_cross_product(const fabrica_Vec3I *a, const fabrica_Vec3I *b,
-                                 fabrica_Vec3I *out) {
-    out->x = a->y * b->z - a->z * b->y;
-    out->y = a->z * b->x - a->x * b->z;
-    out->z = a->x * b->y - a->y * b->x;
-}
--- fabrica/math/vec3i.h
+++ fabrica/math/vec3i.h
@@ -13,7 +13,5 @@ void fabrica_vec3i_add(const fabrica_Vec3I *a, const fabrica_Vec3I *b,
                        fabrica_Vec3I *out);
 void fabrica_vec3i_scale(const fabrica_Vec3I *a, int32_t scalar,
                          fabrica_Vec3I *out);
-void fabrica_vec3i_cross_product(const fabrica_Vec3I *a, const fabrica_Vec3I *b,
-                                 fabrica_Vec3I *out);
 
 #endif
